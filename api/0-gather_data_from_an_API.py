cdb94a610caa44b288f37f670bf3d3c28a3c1977
Meashyshy <camillefvrl@gmail.com>
2023-08-21 06:48:47 -0700

    PEP8 Validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,9 +23,10 @@ def employee_todo(employee_id):
     completed_tasks = [todo for todo in todos_data if todo["completed"]]
     num_completed_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print(f"\t {task['title']}")  # Add a space before the task title
 
 
 if __name__ == "__main__":
